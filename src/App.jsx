18d824c200c3561a7a9514686cf8165c078ad627
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-01-27 22:01:41 -0800

    kdhfgksdjfhglksdfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Analytics } from "@vercel/analytics/react"
 
 import Header from './Components/Header'
 import HtmlCanvas from './Components/htmlCanvas'
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Analytics />
       <Header />
       <HtmlCanvas width={200} height={100} />
     </>
