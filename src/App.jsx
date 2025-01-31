948a36cf45b3662d04b43485a79d8ff70e9d1211
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-01-30 21:55:08 -0800

    Simplified Layout

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,15 @@ function App() {
       <div className="App" style={{ display: "flex", flexDirection: "column", width: "100%", alignItems: "center", textAlign: "center" }}>
 
         <Header />
-        <div className="NameTag" style={{ position: "absolute", width: "auto", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", textAlign: "left", zIndex: "1", filter: "invert(100%)", mixBlendMode: 'difference', color: "black" }}>
-          <div style={{ display: "flex", flexDirection: "column-reverse", alignItems: "center", textAlign: "center", width: "100%", margin: "0", padding: "0" }}>
+        <div className="NameTag" style={{ width: "auto", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", textAlign: "left", zIndex: "1", filter: "invert(100%)", mixBlendMode: 'difference', color: "black" }}>
 
-            <h1 style={{ fontSize: "100px", margin: "0", padding: "0", backgroundColor: "black", color: "white", width: "fit-content" }}>Chase Nagle.</h1>
+          <h1 style={{ fontSize: "100px", margin: "0", padding: "0", backgroundColor: "black", color: "white", width: "fit-content" }}>Chase Nagle.</h1>
 
-          </div>
-          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "left", textAlign: "left", width: "100%", margin: "0", padding: "0", lineHeight: "1rem" }}>
-            <p style={{}}>Java/TypeScript - C/C++ - Java - C# - Python - Go - Node.js - mongoDB - AWS - Open/WebGL - Vulkan</p>
-          </div>
+          <p style={{}}>Java/TypeScript - C/C++ - Java - C# - Python - Go - Node.js - mongoDB - AWS - Open/WebGL - Vulkan</p>
 
         </div>
 
-        <h1 style={{ filter: "invert(100%)", mixBlendMode: "difference", position: "fixed", bottom: "0", left: "0", margin: "10px", fontSize: "10px", color: "black", zIndex: "1", fontWeight: "lighter", writingMode: "vertical-rl" }}>Oakland, CA - @2025 </h1>
+        <div style={{ filter: "invert(100%)", mixBlendMode: "difference", position: "fixed", bottom: "0", left: "0", margin: "10px", fontSize: "15px", color: "black", zIndex: "1", fontWeight: "lighter", lineHeight: "1em" }}><p>Oakland, CA</p> <p>2025</p> </div>
 
 
 
@@ -39,11 +35,9 @@ function App() {
         <HtmlCanvas tipColor={[1.0, 0.0, 0.0]} bodyColor={[0.0, 0.0, 0.0]} dist={75.0} scroll={0.1} />
 
         <HtmlCanvas tipColor={[1.0, 0.0, 0.0]} bodyColor={[1.0, 1.0, 1.0]} dist={0.0} scroll={0.01} />
-        <div style={{ height: "90vh", width: "100vw" }}></div>
         <h1 style={{ color: "black", filter: "invert(100%)", mixBlendMode: "difference" }}>Projects</h1>
 
       </div >
-      <div style={{ height: "80vh", width: "100vw" }}></div>
 
 
 
